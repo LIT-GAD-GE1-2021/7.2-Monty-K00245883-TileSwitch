43a9c9293d2cf696d1eb2dd47259898af01874ed
montypyle <77437020+montypyle@users.noreply.github.com>
2021-02-24 20:51:32 +0000

    Initial commit

---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            ToggleSwitch();
+        }
+    }
+    public void ToggleSwitch()
+    {
+        if (this.name == "GreenSwitch")
+        {
+            TileSwitchLM.instance.greenSwitchOff = !TileSwitchLM.instance.greenSwitchOff;
+        }
+        if (this.name == "RedSwitch")
+        {
+            TileSwitchLM.instance.redSwitchOff = !TileSwitchLM.instance.redSwitchOff;
+        }
+    }
+}
---
+++ Assets/Scripts/TileSwitchLM.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSwitchLM : MonoBehaviour
+{
+    public static TileSwitchLM instance;
+
+    public GameObject greenWallMap;
+    public GameObject redWallMap;
+    public bool greenSwitchOff;
+    public bool redSwitchOff;
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        greenSwitchOff = true;
+        redSwitchOff = true;
+    }
+
+    void Update()
+    {
+        greenWallMap.SetActive(greenSwitchOff);
+        redWallMap.SetActive(redSwitchOff);
+    }
+
+}
